5a4189e7453765d36c97ebd8b1a9c66fe3f14d72
dastgir333 <dastgir333@gmail.com>
2018-04-21 19:28:42 +0530

    Add files via upload

---
+++ assignment13.2/src/app/routing/routing.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserDataComponent } from 'app/user-data/user-data.component';
+import { UserListComponent } from 'app/user-list/user-list.component';
+import { UserDetailComponent } from "app/user-detail/user-detail.component";
+import { LoginComponent } from "app/login/login.component";
+import { AuthGuardComponent } from "app/service/auth-guard.service";
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'user', component: UserDataComponent, canActivate: [AuthGuardComponent] },
+  { path: 'userList', component: UserListComponent },
+  { path: 'userDetail/:id', component: UserDetailComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/user' },
+  { path: '**', component: UserDataComponent }
+];
+
+export const routedComponents = RouterModule.forRoot(routes)
